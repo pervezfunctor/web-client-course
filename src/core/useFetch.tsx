898495742b5075be2b87f6b8c778cb09a620edab
Pervez Iqbal <pervezfunctor@gmail.com>
2022-08-19 22:59:45 +0530

    add useFetch

---
+++ src/core/useFetch.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import axios from 'redaxios'
+import { reducer, useReducer } from '../reducer'
+import { delay } from './delay'
+
+type FetchState = Readonly<{
+  data?: unknown
+  error?: unknown
+  isLoading: boolean
+}>
+
+const initial: FetchState = {
+  data: undefined,
+  error: undefined,
+  isLoading: true,
+}
+
+const slice = reducer(initial, {
+  success(draft, data: unknown) {
+    draft.data = data
+    draft.error = undefined
+    draft.isLoading = false
+  },
+  error(draft, error: unknown) {
+    draft.error = error
+    draft.isLoading = false
+  },
+})
+
+export function useFetch(url: string): FetchState {
+  const [state, actions] = useReducer(slice)
+
+  React.useEffect(() => {
+    let cancelled = false
+
+    delay(5000)
+      .then(async () => axios.get(url))
+      .then(res => {
+        if (!cancelled) {
+          actions.success(res.data)
+        }
+      })
+      .catch(err => {
+        if (!cancelled) {
+          actions.error(err)
+        }
+      })
+
+    return () => {
+      cancelled = true
+    }
+  }, [url])
+
+  return state
+}
