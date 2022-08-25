dab130201ca18633efe098e2c5de713504db50a1
Pervez Iqbal <pervezfunctor@gmail.com>
2022-08-25 19:10:04 +0530

    reducerHook counter example

---
+++ src/examples/useCounter.tsx
@@ -0,0 +1,27 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import React from 'react'
+import { reducerHook } from '../reducer'
+
+export const useCounter = reducerHook(
+  { count: 0 },
+  {
+    onInc(state) {
+      state.count++
+    },
+    onDec(state) {
+      state.count--
+    },
+  },
+)
+
+export const Counter = () => {
+  const [state, { onDec, onInc }] = useCounter()
+
+  return (
+    <Box>
+      <Button onClick={onInc}>+</Button>
+      <Text>{state.count}</Text>
+      <Button onClick={onDec}>-</Button>
+    </Box>
+  )
+}
