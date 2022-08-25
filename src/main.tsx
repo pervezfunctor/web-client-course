118d803aa27be5bb42e200b0f4903e9e5b56eb4b
Pervez Iqbal <pervezfunctor@gmail.com>
2022-08-25 19:10:47 +0530

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,17 @@
-import { Box, Container, Input, Text } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
-
+import { ChakraProvider, Container } from '@chakra-ui/react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { reducerHook } from './reducer'
 import './index.css'
+import { TodoApp } from './use-query'
 
 const root = createRoot(document.getElementById('root')!)
 
-const initial = { step: 1, count: 0 }
-
-const useStepCount = reducerHook(initial, {
-  next(draft) {
-    draft.count = draft.count + draft.step
-  },
-
-  setStep(draft, step: number) {
-    draft.step = step
-  },
-})
-
-const Counter = () => {
-  const [{ count }, { next, setStep }] = useStepCount()
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      next()
-    }, 1000)
-
-    return () => clearInterval(id)
-  }, [])
-
-  return (
-    <Box>
-      <Text>{count}</Text>
-      <Input onChange={evt => setStep(+evt.target.value)} />
-    </Box>
-  )
-}
-
 root.render(
-  <Container>
-    <React.StrictMode>
-      <Counter />
-    </React.StrictMode>
-  </Container>,
+  <ChakraProvider>
+    <Container>
+      <React.StrictMode>
+        <TodoApp />
+      </React.StrictMode>
+    </Container>
+  </ChakraProvider>,
 )
