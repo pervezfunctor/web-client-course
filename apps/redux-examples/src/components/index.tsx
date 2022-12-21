2ef9381f58ad54087e39fe406e63dd75d3e2dd1d
Pervez Iqbal <pervezfunctor@gmail.com>
2022-12-21 19:26:41 +0530

    redux examples as a separate project

--- apps/redux-examples/src/components/index.tsx
+++ apps/redux-examples/src/components/index.tsx
@@ -1,4 +1,3 @@
-import { Container } from '@chakra-ui/react'
 import React from 'react'
 import { ReducerProvider } from './Context'
 import { TodoList } from './TodoList'
@@ -6,9 +5,7 @@ import { TodoList } from './TodoList'
 export const TodoApp = () => {
   return (
     <ReducerProvider>
-      <Container>
-        <TodoList />
-      </Container>
+      <TodoList />
     </ReducerProvider>
   )
 }
